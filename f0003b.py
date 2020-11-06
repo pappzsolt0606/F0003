4edea7885991da15a3bc719bfe3f688e05a2c95e
pappzsolt0606 <zsolt.papp0606@gmail.com>
2020-11-06 07:42:13 +0000

    Initial commit

---
+++ f0003b.py
@@ -0,0 +1,3 @@
+év = input('Melyik évben születtél? ')
+csillagjegy = input ('Melyik csillagjegyben születtél? ')
+print('Az Úr ', év, '. évében születtél, a ', csillagjegy,  ' jegyében. ', sep='')
---
+++ main.py
@@ -0,0 +1,2 @@
+import f0003a
+import f0003b
